dca083c5a07fef6100d88ec8a3ccfcc5da296380
Emmanuel <kipronofb@outlook.com>
2023-07-07 20:15:01 +0300

    UPDATE: add bing authentication

--- src/components/resume/personalInfo/components/headline.tsx
+++ src/components/resume/personalInfo/components/headline.tsx
@@ -8,7 +8,7 @@ export function Headline() {
     return (
         <Box>
         <Stack  >
-            <Typography variant="h3" margin={2} className="animate__animated animate__fadeIn animate__fadeInUp">
+            <Typography variant="h1" sx={{fontSize: "60px"}} margin={2} className="animate__animated animate__fadeIn animate__fadeInUp">
                 {info.name} 
             </Typography>
             <Typography margin={2} variant="h5" className="animate__animated animate__fadeIn animate__fadeInUp"> 
