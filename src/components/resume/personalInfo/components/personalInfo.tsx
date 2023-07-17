bd2b4fbf077a58508e4261a38b1a8a1efbfd8352
Emmanuel <kipronofb@outlook.com>
2023-07-17 13:59:36 +0300

    UPDATE: lazy load profile image

--- src/components/resume/personalInfo/components/personalInfo.tsx
+++ src/components/resume/personalInfo/components/personalInfo.tsx
@@ -1,32 +1,10 @@
 import { Stack } from "@mui/material";
 import {Info,Headline} from "./index"
 import React from "react";
-import { Rings } from  'react-loader-spinner'
 
 
 const Image = React.lazy(()=>import("./image"))
 
-const Loading = () => (
-    <Stack
-        flexDirection={"row"}
-        justifyContent={"center"}
-        alignItems={"center"}
-        height={"100%"}
-        width={"100%"}
-    >
-        <Rings
-            height="80"
-            width="80"
-            color="#4fa94d"
-            radius="6"
-            wrapperStyle={{}}
-            wrapperClass="div"
-            visible={true}
-            ariaLabel="rings-loading"
-        />
-    </Stack>
-    
-)
 
 
 export default function PersonalInfo() {
